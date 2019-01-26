d3b754ae9cb5372580325d3f9390d74c66876948
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2019-01-26 16:10:54 +0100

    Création des classes Tower et Monster

---
+++ src/towerINT/Monster.java
@@ -0,0 +1,37 @@
+package towerINT;
+
+/**Définit les différents types de monstres*/
+enum monsterType{   //TODO: ajouter d'autres types
+    zombie,
+    squelette;
+}
+
+public class Monster {
+    private double pv;
+    private double resistance;
+    private double vitesse;
+
+    Monster(monsterType t){
+        switch (t){ //TODO: implémenter d'autres types
+                    //TODO: mettre des valeurs cohérentes
+            case zombie:
+                pv=100;
+                resistance=10;
+                vitesse=10;
+                break;
+            case squelette:
+                pv=50;
+                resistance=20;
+                vitesse=20;
+                break;
+        }
+    }
+
+    /**Retire des PV au monstre et renvoie true s'il est toujours vivant*/
+    boolean prendDegats(double degats){
+        pv-=degats/resistance;
+        return pv>0;
+    }
+
+    //TODO: créer une méthode de déplacement
+}
---
+++ src/towerINT/Tower.java
@@ -0,0 +1,30 @@
+package towerINT;
+
+/**Définit les différents types de tours*/
+enum TowerType{ //TODO: Donner de vrais noms aux tours
+    Type1,
+    Type2;
+}
+public class Tower {
+    private double portee;
+    private double puissance;
+    private double rapidite;
+    private double cout;
+
+    Tower(TowerType t){
+        switch (t){ //TODO: donner des valeurs cohérentes aux champs
+            case Type1:
+                portee=10;
+                puissance=10;
+                rapidite=10;
+                cout=10;
+                break;
+            case Type2:
+                portee=20;
+                puissance=20;
+                rapidite=20;
+                cout=20;
+                break;
+        }
+    }
+}
