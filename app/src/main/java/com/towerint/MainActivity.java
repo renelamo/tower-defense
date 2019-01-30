ca4e85e737acd57028b841893d8d7b3ec865e3ae
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2019-01-30 10:39:00 +0100

    connection du bouton Exit

--- app/src/main/java/com/towerint/MainActivity.java
+++ app/src/main/java/com/towerint/MainActivity.java
@@ -1,7 +1,12 @@
 package com.towerint;
 
+import android.app.Activity;
+import android.app.Application;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button exitButton=findViewById(R.id.exitButton);
+        /**Définition de la fonction appelée par exitButton*/
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
