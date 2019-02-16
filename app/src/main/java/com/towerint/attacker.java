ba668299f4207bf1465eea9020168d861d479b26
naruaxe94 <naruaxe94@github.com>
2019-02-16 22:58:11 +0100

    Création de la classe attacker

---
+++ app/src/main/java/com/towerint/attacker.java
@@ -0,0 +1,12 @@
+package com.towerint;
+
+abstract class tower {
+    private int radius;
+    private int range;
+    private int speed;
+    private int cost;
+    //private image; TODO créer une classe pour l'image
+    private projectile projectile; // TODO créer classe projectile
+    private int popularity_max;
+    private int ProbabilityLoosePopularity;
+}
