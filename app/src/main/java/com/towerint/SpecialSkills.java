05eaaf697cff1218a1b96191df9774b067081f4c
naruaxe94 <naruaxe94@github.com>
2019-02-17 01:11:55 +0100

    mise en place de la classe compétence spéciale

---
+++ app/src/main/java/com/towerint/SpecialSkills.java
@@ -0,0 +1,21 @@
+package com.towerint;
+
+public class SpecialSkills {
+    private double x;
+    private double y;
+    private double radius;
+    private double popularityCost;
+
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public double getPopularityCost(){
+        return popularityCost;
+    }
+}
