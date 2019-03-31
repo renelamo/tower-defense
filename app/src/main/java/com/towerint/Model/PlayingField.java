7bef4f6e5019dd683192478cda51c715f0251b87
naruaxe94 <naruaxe94@github.com>
2019-03-31 04:50:47 +0200

    constructeur de PlayingField

--- app/src/main/java/com/towerint/Model/PlayingField.java
+++ app/src/main/java/com/towerint/Model/PlayingField.java
@@ -7,4 +7,13 @@ abstract class PlayingField {
     private double yMax;
     private Way way;
     private double weigthWay;
+
+    PlayingField(Way way){
+        xMin=0;
+        yMin=0;
+        xMax=100;
+        yMax=50;
+        weigthWay=5;
+        this.way=way;
+    }
 }
