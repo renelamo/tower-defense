b73794dc5d9e9303209f62ce43fe440c7ac303e1
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2019-04-01 17:37:12 +0200

    ajout des fichiers de jeu

---
+++ app/src/main/java/com/towerint/PlayingField.java
@@ -0,0 +1,10 @@
+package com.towerint;
+
+abstract class PlayingField {
+    private double xMin;
+    private double xMax;
+    private double yMin;
+    private double yMax;
+    private Way way;
+    private double weigthWay;
+}
