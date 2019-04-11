24bc71b5dfa2dd77ee6b899dd1e1b085dd742b93
Mikail <mikail.duzenli@telecom-sudparis.eu>
2019-04-11 13:14:17 +0200

    création de movable et attacker hérite de movable

--- app/src/main/java/com/towerint/Model/Attacker.java
+++ app/src/main/java/com/towerint/Model/Attacker.java
@@ -4,15 +4,12 @@ import com.towerint.Controller.GameEngine;
 
 import static com.towerint.Controller.GameEngine.FPS;
 
-public abstract class Attacker extends Printable {
-    private double radius;
-    private double range;
+public abstract class Attacker extends Movable {
     private Vector2 speed;//Vitesse réelle en px par frame
     private float maxSpeed;//En module (en px/frame)
     private Node node; //Noeud vers lequel il se déplace
-    private double attackCooldown;
     private int health;
-    private Projectile projectile; // TODO créer classe Projectile
+    private Projectile projectile; // TODO finir classe Projectile
     private GameEngine parent;
 
 
@@ -20,72 +17,15 @@ public abstract class Attacker extends Printable {
     @Deprecated
     Attacker(int posX, int posY, GameEngine parent, int resource){
         super(posX, posY,parent, resource);
-        this.parent=parent;
-        speed=new Vector2();
     }
     Attacker(Way toFollow, GameEngine parent, int resource, float maxSpeed){
-        super(parent, resource);
-        this.maxSpeed=maxSpeed/FPS;
-        speed=new Vector2();
-        this.follow(toFollow);
+        super(toFollow, parent, resource, maxSpeed);
     }
 
     ///////////GETTERS////////////////////////////////////////////////
-    public double getRadius(){
-        return radius;
-    }
-    public double getRange(){
-        return range;
-    }
-    public double getAttackCooldown(){
-        return attackCooldown;
-    }
+
     public Projectile getProjectile(){
         return projectile;
     }
 
-    ///////////SETTERS///////////////////////////////////////////////
-
-    public void setSpeed(float x, float y){ //En px/s
-        speed.setC(x/FPS,y/FPS);
-        speedToFace();
     }
-
-    public void setSpeed(Vector2 v){
-        float n=maxSpeed/v.getNorm();
-        setSpeed(v.getX()*n,v.getY()*n);
-    }
-
-    public void setSpeed(float s){//en px/s
-        s/=FPS;//en px/frame
-        if(speed.getNorm()!=0) {
-            speed.mult(s / speed.getNorm()/*en px/frame*/);
-        }
-        maxSpeed=s;
-    }
-
-    ///////////////AUTRES METHODES///////////////////////////
-
-    public void move(){
-        setPos(this.getPosition().add(speed));
-        //Si la distance entre l'attaquant et le prochain noeud est inférieure à la moitié de la distance parcourue en 1 frame, on se dirrige vers le noeud suivant;
-        if(node.getPosition().diff(this.getPosition()).getNorm()<speed.getNorm()/2){ //TODO: je ne sais pas pourquoi speed.getNorm() est différent (environ 25 fois inférieur) de maxSpeed()
-            if(!node.hasNext()){
-                setSpeed(0,0);
-            }else {
-                setSpeed(node.getDirection());
-                node = node.getNext();
-            }
-        }
-    }
-
-    public void speedToFace(){
-        this.setRotation(speed.getTheta());
-    }
-
-    public void follow(Way w){
-        node=w.getFirstNode().getNext();
-        boolean reussite =setPos(w.getFirstNode().getPosition());
-        setSpeed(w.getFirstNode().getDirection());
-    }
-}
--- app/src/main/java/com/towerint/Model/Projectile.java
+++ app/src/main/java/com/towerint/Model/Projectile.java
@@ -1,15 +1,17 @@
 package com.towerint.Model;
 
 import com.towerint.Controller.GameEngine;
+import com.towerint.R;
 
-abstract class Projectile extends Printable {
+abstract class Projectile extends Movable {
+    //TODO Faire en sorte que la classe hérite proprement de Movable
     private Vector2 position;
     private double speed; // la vitesse à laquelle se déplace le projectile
     private double power;
-    //private image; TODO créer une classe pour l'image
+    private static final int resource= R.drawable.kisspng_missile_weapon_computer_icons_projectile_missile_turret_5b22188ee4cce9;
 
-    public Projectile(GameEngine parent, int resource){
-        super(parent, resource);
+    public Projectile(int posX, int posY,GameEngine parent, int resource){
+        super(posX, posY, parent, resource);
     }
     public double getSpeed(){
         return speed;
@@ -17,5 +19,9 @@ abstract class Projectile extends Printable {
     public double getPower(){
         return power;
     };
+
+    public void setSpeed(double speed){ //A utiliser pour la vitesse de tire des differentes tour
+        this.speed=speed;
+    }
 
 }
